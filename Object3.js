df78b73ddf79b27dc6207ef2ee4e353492c3451a
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2021-09-13 21:50:01 +0530

    Create Object3.js

---
+++ Object3.js
@@ -0,0 +1,49 @@
+<html>  
+    <head>
+
+
+
+
+<title>NEW</title>
+
+
+
+
+    </head> 
+    <body>
+
+
+
+
+        <p>
+            The HyperText Markup Language, or HTML is the standard markup language for documents designed to be displayed in a web browser. It can be assisted by technologies such as Cascading Style Sheets and scripting languages such as JavaScript
+        </p>
+        <script > 
+        
+        
+        let Pavan=
+                  {
+                     name:'Pavan',   
+                     age:23,
+                     college:'CBIT',
+                     subjects:['Mechanics','Drawing','Machine Tools','Machine Design']
+
+                  }
+                  console.log(Pavan.name);
+                  console.log(Pavan.age);
+                  console.log(Pavan.college);
+                  console.log(Pavan.subjects);
+
+        
+        
+        
+        
+        
+      </script>
+    </body>
+
+
+
+
+
+</html>
