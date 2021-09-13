442e20a3d46ea8708a45df43feb91d07e83afcc1
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2021-09-13 16:01:11 +0530

    Create object.js

---
+++ object.js
@@ -0,0 +1,32 @@
+<html>  
+    <head>
+
+
+
+
+<title>NEW</title>
+
+
+
+
+    </head> 
+    <body>
+
+
+
+
+        <p>
+            The HyperText Markup Language, or HTML is the standard markup language for documents designed to be displayed in a web browser. It can be assisted by technologies such as Cascading Style Sheets and scripting languages such as JavaScript
+        </p>
+        <script src="numbers.js"> </script>
+    </body>
+
+
+
+
+
+</html>
+let numbers=[1,4,6,9,7];
+console.log(numbers[1]);
+numbers.push(6);
+console.log(numbers);
