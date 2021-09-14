a430b79db0fdc8786a170b41a6867396a0985d8e
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2021-09-14 12:41:00 +0530

    Create for_loop.js

---
+++ for_loop.js
@@ -0,0 +1,42 @@
+for(i=0;i<5;i++)
+{
+
+
+    console.log("The number iss"+"   "+i);
+}
+
+for(let x=o;x<50;x++)
+{
+
+ console.log("I am sorry Harry Porter");
+
+}
+======================================================================================================================================================================
+  <html>  
+    <head>
+
+
+
+
+<title>NEW</title>
+
+
+
+
+    </head> 
+    <body>
+
+
+
+
+        <p>
+            The HyperText Markup Language, or HTML is the standard markup language for documents designed to be displayed in a web browser. It can be assisted by technologies such as Cascading Style Sheets and scripting languages such as JavaScript
+        </p>
+        <script src="for.js"> </script>
+    </body>
+
+
+
+
+
+</html>
