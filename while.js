1643bb4b5d4272da8eb0378c062c61d662dc85bf
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2021-09-14 12:11:05 +0530

    Create while.js

---
+++ while.js
@@ -0,0 +1,38 @@
+let i=0;
+while(i<5)
+{
+    
+    console.log('The number is '+"  "+ i);
+    i++;
+
+
+}
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+<html>  
+    <head>
+
+
+
+
+<title>NEW</title>
+
+
+
+
+    </head> 
+    <body>
+
+
+
+
+        <p>
+            The HyperText Markup Language, or HTML is the standard markup language for documents designed to be displayed in a web browser. It can be assisted by technologies such as Cascading Style Sheets and scripting languages such as JavaScript
+        </p>
+        <script src="while.js"> </script>
+    </body>
+
+
+
+
+
+</html>
