f495255f49addd09f2dbbc9b51d82014d24339e9
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2021-09-14 11:39:01 +0530

    Create functions_if.js

---
+++ functions_if.js
@@ -0,0 +1,35 @@
+let numbers=[];
+numbers=[5,2,5,4,7,9];
+if(numbers[0]==numbers[2])
+{
+   console.log('yeah it is correct');
+}   
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  <html>  
+    <head>
+
+
+
+
+<title>NEW</title>
+
+
+
+
+    </head> 
+    <body>
+
+
+
+
+        <p>
+            The HyperText Markup Language, or HTML is the standard markup language for documents designed to be displayed in a web browser. It can be assisted by technologies such as Cascading Style Sheets and scripting languages such as JavaScript
+        </p>
+        <script src="if.js"> </script>
+    </body>
+
+
+
+
+
+</html>
