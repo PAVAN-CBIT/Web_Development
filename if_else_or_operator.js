e046350886a6aedc1f60fee94aeecf923d2024ba
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2021-09-14 11:54:14 +0530

    Create if_else_or_operator.js

---
+++ if_else_or_operator.js
@@ -0,0 +1,17 @@
+let numbers=[];
+numbers=[5,2,5,4,7,5];
+if(numbers[0]==numbers[2])
+{
+   console.log('yeah it is correct');
+}   
+if(numbers[0]==numbers[4]|| numbers[2]==numbers[5])
+{
+
+  console.log('correct');
+
+
+}
+else{
+
+    console.log('wrong,try again')
+}
