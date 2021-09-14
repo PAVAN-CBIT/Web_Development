adf7a85ea7a7e473c59b8b2e67216b77d9e06ba1
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2021-09-14 12:56:19 +0530

    Create Switch2.js

---
+++ Switch2.js
@@ -0,0 +1,52 @@
+
+let games ='foosball'
+switch(games)
+{
+case'cricket':
+console.log("i love cricket")
+break;
+case'carrom':
+console.log("i love carrom")
+break;
+case'football':
+console.log("i love football")
+break;
+default:
+    console.log("none")
+    break;
+
+
+
+}
+
+
+=============================================================================================================================================================================
+
+<html>  
+    <head>
+
+
+
+
+<title>NEW</title>
+
+
+
+
+    </head> 
+    <body>
+
+
+
+
+        <p>
+            The HyperText Markup Language, or HTML is the standard markup language for documents designed to be displayed in a web browser. It can be assisted by technologies such as Cascading Style Sheets and scripting languages such as JavaScript
+        </p>
+        <script src="switch.js"> </script>
+    </body>
+
+
+
+
+
+</html>
