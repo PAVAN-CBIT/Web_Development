cb3750826da4b5581dbac52eb99b637ed20c2cf3
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2021-09-16 12:38:41 +0530

    Create jQuery_selectors.js

---
+++ jQuery_selectors.js
@@ -0,0 +1,35 @@
+<html>  
+    <head>
+
+
+
+
+<title>NEW</title>
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"</script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui-min.js"
+         integrity="sha256-VazP97ZCwtekAsvgPBSUwpFkdrwD3unUfSGVYrahUqU="
+         crossorigin="anonymous"></script>
+         <link rel="stylesheet" href="https://code.jquery.com/ui/1.10.4/themes/ui-lightness/jquery-ui.css">
+
+
+
+
+
+    </head> 
+    <body>
+
+
+
+<h1>BIG BOSS</h1>
+  <h2>below that see</h2>
+        <p>
+            The HyperText Markup Language, or HTML is the standard markup language for documents designed to be displayed in a web browser. It can be assisted by technologies such as Cascading Style Sheets and scripting languages such as JavaScript
+        </p>
+        
+      </body>
+
+
+
+        
+
+</html>
