033747c9423db05cf014c1bc94718fa25ffc0645
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2021-09-16 21:52:34 +0530

    Create after()_jquery.js

---
+++ after()_jquery.js
@@ -0,0 +1,41 @@
+<html>  
+    <head>
+
+
+
+
+<title>NEW</title>
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"</script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui-min.js"
+         integrity="sha256-VazP97ZCwtekAsvgPBSUwpFkdrwD3unUfSGVYrahUqU="
+         crossorigin="anonymous"></script>
+         <link rel="stylesheet" href="https://code.jquery.com/ui/1.10.4/themes/ui-lightness/jquery-ui.css">
+
+
+
+
+
+    </head> 
+    <body>
+
+
+
+<h1>jQuery</h1>
+  
+        <p>
+            jQuery is a JavaScript library designed to simplify HTML DOM tree traversal and manipulation, as well as event handling, CSS animation, and Ajax. It is free, open-source software using the permissive MIT License. As of May 2019, jQuery is used by 73% of the 10 million most popular websites.
+        </p>
+        <script>
+            
+            $("p").css("background","yellow")
+            $("h1").after("<h2>I N T R O D U C T I O N:-</h2>")
+            
+            </script>
+        
+      </body>
+
+
+
+        
+
+</html>
