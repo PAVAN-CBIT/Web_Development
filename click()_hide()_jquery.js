c496b5dd818ebaacf5173deeef59bd4b4cd1f14d
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2021-09-17 16:23:00 +0530

    Create click()_hide()_jquery.js

---
+++ click()_hide()_jquery.js
@@ -0,0 +1,63 @@
+<html>
+<head>
+  <style>
+  .styleclass
+  {
+
+    border: 5px solid green;
+
+  }
+  
+  
+  </style>
+    
+      <title>jQuery </title>
+
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"</script>
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui-min.js"
+                 integrity="sha256-VazP97ZCwtekAsvgPBSUwpFkdrwD3unUfSGVYrahUqU="
+                 crossorigin="anonymous"></script>
+                 <link rel="stylesheet" href="https://code.jquery.com/ui/1.10.4/themes/ui-lightness/jquery-ui.css">
+        
+
+
+</head>
+
+
+
+
+
+
+<body>
+     <h1>THE WORLD’S 10 HIGHEST-PAID ATHLETES SCORE $4.2 BILLION IN 2021</h1>
+     <div id="atl">
+      <img src="messi.jpg" >
+      <img src="ronaldo.jpg" >
+      <img src="neymar.jpg" >
+      
+      </div>
+  
+    
+    
+
+</body>
+
+
+   <script>  
+    $(document).ready(function() {
+      $("img").click(function() {
+        $(this).hide();
+        
+      });
+    });
+  </script>
+<style>
+  
+
+
+
+
+
+
+
+</html>
