e873d2c3f6c3ed7c0f6aed0af7cecbacdd9c289c
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2021-09-17 11:25:57 +0530

    Create css()_jquery.js

---
+++ css()_jquery.js
@@ -0,0 +1,60 @@
+<html>
+<head>
+      <title>jQuery </title>
+
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"</script>
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui-min.js"
+                 integrity="sha256-VazP97ZCwtekAsvgPBSUwpFkdrwD3unUfSGVYrahUqU="
+                 crossorigin="anonymous"></script>
+                 <link rel="stylesheet" href="https://code.jquery.com/ui/1.10.4/themes/ui-lightness/jquery-ui.css">
+        
+
+
+</head>
+
+
+
+
+
+
+<body>
+<h1>1.jQuery</h1>
+<h2>I N T R O D U C T I O N : --</h2>
+<p>    jQuery is a JavaScript library designed to simplify HTML DOM tree traversal and manipulation, as well as event handling, CSS animation, and Ajax.[3] It is free, open-source software using the permissive MIT License.[4] As of May 2019, jQuery is used by 73% of the 10 million most popular websites.[5] Web analysis indicates that it is the most widely deployed JavaScript library by a large margin, having at least 3 to 4 times more usage than any other JavaScript library.[5][6]
+
+jQuery's syntax is designed to make it easier to navigate a document, select DOM elements, create animations, handle events, and develop Ajax applications. jQuery also provides capabilities for developers to create plug-ins on top of the JavaScript library. This enables developers to create abstractions for low-level interaction and animation. </p>
+
+<script>       
+    
+    
+    let design=
+    {
+      color:"green",
+      background:"yellow",
+      border:"2px solid black"
+
+
+    }
+    $("p").css(design)
+    
+    
+    </script>
+
+
+
+
+
+
+
+
+
+</body>
+
+
+
+
+
+
+
+
+</html>
