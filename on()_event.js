a1d7be33edd884e8f3f8fc3d8e603c669eeb86fb
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2021-09-17 16:35:15 +0530

    Create on()_event.js

---
+++ on()_event.js
@@ -0,0 +1,58 @@
+<html>
+<head>
+  <style>
+  .styleclass
+  {
+
+    border: 5px solid green;
+
+  }
+  
+  
+  </style>
+    
+      <title>jQuery </title>
+
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"</script>
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui-min.js"
+                 integrity="sha256-VazP97ZCwtekAsvgPBSUwpFkdrwD3unUfSGVYrahUqU="
+                 crossorigin="anonymous"></script>
+                 <link rel="stylesheet" href="https://code.jquery.com/ui/1.10.4/themes/ui-lightness/jquery-ui.css">
+        
+
+
+</head>
+
+
+
+
+
+
+<body>
+     <h1>THE WORLD’S 10 HIGHEST-PAID ATHLETES SCORE $4.2 BILLION IN 2021</h1>
+     <p>Hello world I"m Messi...!</p>
+  
+    <input type="text" id="press">
+    
+
+</body>
+
+
+   <script>  
+    $(document).ready(function() {
+      $("input").on("keypress",function() {
+        $("p").hide();
+        
+      });
+    });
+  </script>
+<style>
+  
+
+
+
+
+
+
+
+</html>
