c3e6556c6d7e4164a8ac2fe00a83a0abca9642eb
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2021-09-17 22:25:50 +0530

    Create draggable().js

---
+++ draggable().js
@@ -0,0 +1,53 @@
+<html>
+<head>
+  
+    
+      <title>jQuery </title>
+
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"</script>
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui-min.js"
+                 integrity="sha256-VazP97ZCwtekAsvgPBSUwpFkdrwD3unUfSGVYrahUqU="
+                 crossorigin="anonymous"></script>
+                 <link rel="stylesheet" href="https://code.jquery.com/ui/1.10.4/themes/ui-lightness/jquery-ui.css">
+        
+
+
+</head>
+
+
+
+
+
+
+<body>
+  <h1>jQuery Tutorial</h1>
+  <style>
+    #drag
+    {
+      width: 150px;
+      height: 60px;
+      background-color: red;
+    }
+  </style>
+  <script>
+    $(function() 
+    {
+      $("#drag").draggable();
+    });
+        
+    </script>
+    <div id="drag">
+      
+      <p>H E L L O</p>
+      </div>
+
+     
+     
+
+   
+
+
+
+
+
+</html>
