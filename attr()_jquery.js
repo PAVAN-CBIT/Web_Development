396021083674b259ea4781896862407269d67d60
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2021-09-17 12:16:33 +0530

    Create attr()_jquery.js

---
+++ attr()_jquery.js
@@ -0,0 +1,86 @@
+<html>
+<head>
+    <style>
+    .Athelets
+    {
+      float: left;
+    }
+    img
+    {
+
+       height: 300px;
+       width: 250px;
+
+    }
+
+    </style>
+      <title>jQuery </title>
+
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"</script>
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui-min.js"
+                 integrity="sha256-VazP97ZCwtekAsvgPBSUwpFkdrwD3unUfSGVYrahUqU="
+                 crossorigin="anonymous"></script>
+                 <link rel="stylesheet" href="https://code.jquery.com/ui/1.10.4/themes/ui-lightness/jquery-ui.css">
+        
+
+
+</head>
+
+
+
+
+
+
+<body>
+<h1>THE WORLD’S 100 HIGHEST-PAID ATHLETES SCORE $4.2 BILLION IN 2021</h1>
+<img src="ok.jpg">
+
+
+
+
+<h2> Who Were the Highest-Paid Athletes in 2021?</h2>
+
+
+<p>The tally tops Sportico's ranking of the world's 100 highest-paid athletes, besting a pair of global soccer stars, Lionel Messi ($126 million) and Cristiano Ronaldo ($120 million). 
+    ,Neymar jr(100 million)</p>
+<div class="Athelets">
+    
+    <img src="messi.jpg" >
+    <img src="ronaldo.jpg" >
+    <img src="neymar.jpg" >    
+ </div>
+<script>       
+    
+    
+    let design=
+    {
+      color:"green",
+      background:"yellow",
+      border:"2px solid black"
+
+
+    }
+    $("p").css(design)
+    $("img").attr("border","5px solid red")
+    
+    
+    </script>
+
+
+
+
+
+
+
+
+
+</body>
+
+
+
+
+
+
+
+
+</html>
