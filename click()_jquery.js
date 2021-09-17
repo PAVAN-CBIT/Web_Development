8700b299807e9663055784bd57bcf910d353d6ea
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2021-09-17 15:29:56 +0530

    Create click()_jquery.js

---
+++ click()_jquery.js
@@ -0,0 +1,52 @@
+<html>
+<head>
+    
+      <title>jQuery </title>
+
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"</script>
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui-min.js"
+                 integrity="sha256-VazP97ZCwtekAsvgPBSUwpFkdrwD3unUfSGVYrahUqU="
+                 crossorigin="anonymous"></script>
+                 <link rel="stylesheet" href="https://code.jquery.com/ui/1.10.4/themes/ui-lightness/jquery-ui.css">
+        
+
+
+</head>
+
+
+
+
+
+
+<body>
+     <h1>THE WORLD’S 10 HIGHEST-PAID ATHLETES SCORE $4.2 BILLION IN 2021</h1>
+  
+    <input type="text" id="sometext" value=" ">
+    <button>submit</button>
+
+</body>
+<script>
+  
+  $(document).ready(function () {
+    
+  
+    
+    $("button").click(function() 
+    { 
+
+        alert("Value: "+$ ("#sometext").val()); 
+    });
+  });
+  
+  
+  
+  </script>
+
+
+
+
+
+
+
+
+</html>
