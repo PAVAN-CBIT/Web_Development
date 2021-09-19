6cf505dcfad6c3ca97ef3ba73a9f1dfbe52f6fa6
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2021-09-19 15:49:09 +0530

    Create Datepicker().js

---
+++ Datepicker().js
@@ -0,0 +1,36 @@
+<html>  
+  
+<head>
+
+<title>NEW</title>
+
+
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"</script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui-min.js"
+         integrity="sha256-VazP97ZCwtekAsvgPBSUwpFkdrwD3unUfSGVYrahUqU="
+         crossorigin="anonymous"></script>
+         <link rel="stylesheet" href="https://code.jquery.com/ui/1.10.4/themes/ui-lightness/jquery-ui.css">
+
+
+
+</head> 
+
+<body>
+
+<h1>jQuery</h1>
+<input id="date" type="text" size="10" placeholder="Choose the date">
+<script>
+    
+    $("#date").datepicker()
+    </script>
+
+
+
+          
+</body>
+
+
+
+        
+
+</html>
