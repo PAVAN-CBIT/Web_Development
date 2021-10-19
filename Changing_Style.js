cff3315a9296693a93ccb5630ca852b1a4b7cafa
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2021-10-19 22:04:21 +0530

    Create Changing_Style.js

---
+++ Changing_Style.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>D O M</title>
+</head>
+<body>
+
+
+<p id="kp">Hello World..!</p>
+
+<script type="text/javascript">
+		
+  document.getElementById("kp").style.color="red";
+		 
+
+</script>
+	
+
+</body>
+</html>
