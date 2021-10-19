69542ddb5cd356bef1d7115856a5d39a63f1e470
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2021-10-19 22:07:15 +0530

    Create Changing_Content.js

---
+++ Changing_Content.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>D O M</title>
+</head>
+<body>
+
+
+<p id="kp">Hello World..!</p>
+
+<script type="text/javascript">
+		
+  document.getElementById("kp").innerHtml="Welcome back";
+		 
+
+</script>
+	
+
+</body>
+</html>
