fef33adf9e19aad3184a3d07168a9dcb4f7a3884
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2021-10-20 14:29:41 +0530

    Create onclick()_event.js

---
+++ onclick()_event.js
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>D O M</title>
+</head>
+<body>
+	<center>
+
+    <p id="p1"></p>
+    
+   <input type="button" value="Custom" onclick="event()">
+
+     <script type="text/javascript">
+		
+         function  event()
+                { 
+  	              document.getElementById("p1").innerHtml="Welcome";
+
+                }
+		 
+
+    </script>
+	
+</center>
+</body>
+</html>
