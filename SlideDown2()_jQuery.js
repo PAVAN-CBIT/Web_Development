44b094eb042143c08085fdab6519eb17bb4b2d74
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2021-10-21 14:14:09 +0530

    Create SlideDown2()_jQuery.js

---
+++ SlideDown2()_jQuery.js
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>F A D D I N G</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js">
+		
+	</script>
+	<style type="text/css">
+		body
+		{
+          background-color:green;
+
+		}
+		#A
+		{
+
+          background-color: yellow;
+          border: 2px solid red;
+          text-align: center;
+          display: none;
+		}
+	</style>
+</head>
+<body>
+	<center>
+	<button id="kp">S A C H I N</button>
+	
+<p id="A">S A C H I N<    Sachin Ramesh Tendulkar (/ˌsʌtʃɪn tɛnˈduːlkər/ (About this soundlisten); pronounced [sət͡ʃin t̪eːɳɖulkəɾ]; born 24 April 1973) is a former international cricketer of India who served as captain of the Indian national team. He is widely regarded as one of the greatest batsmen in the history of cricket.[6] He is the highest run scorer of all time in international cricket, and the only player to have scored one hundred international centuries, the first batsman to score a double century in a One Day International (ODI), the holder of the record for the most runs in both Test and ODI cricket, and the only player to complete more than 30,000 runs in international cricket.[7] In 2013, he was the only Indian cricketer included in an all-time Test World XI named to mark the 150th anniversary of Wisden Cricketers' Almanack.[8][9][10] He is affectionately known as "Little Master" or "Master Blaster".[11][12][13][14]
+
+Tendulkar took up cricket at the age of eleven, made his Test debut on 15 November 1989 against Pakistan in Karachi at the age of sixteen, and went on to represent Mumbai domestically and India internationally for close to twenty-four years. In 2002, halfway through his career, Wisden Cricketers' Almanack ranked him the second-greatest Test batsman of all time, behind Don Bradman, and the second-greatest ODI batsman of all time, behind Viv Richards.[15] Later in his career, Tendulkar was a part of the Indian team that won the 2011 World Cup, his first win in six World Cup appearances for India.[16] He had previously been named "Player of the Tournament" at the 2003 edition of the tournament, held in South Africa.</p>
+
+
+</center>
+<script type="text/javascript">
+	$(document).ready(function()
+	{
+
+      $("#kp").click(function()
+      {
+        $("#A").slideDown(2000);
+         
+      });
+      
+
+	});
+</script>
+</body>
+</html>
