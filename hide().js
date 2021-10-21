3b0818f379561846875124df16b8bd8f09026a78
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2021-10-21 11:21:10 +0530

    Create hide().js

---
+++ hide().js
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>jQuery</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js">
+		
+	</script>
+</head>
+<body>
+
+<p>Hello world..!</p>
+<button id="hide">Hide</button>
+<button id="show">Show</button>
+
+<script type="text/javascript">
+	
+	$(document).ready(function()
+	{
+      $("#hide").click(function()
+      {
+         $("p").hide(2000);
+      });
+      
+	});
+</script>
+
+</body>
+</html>
